32a74a41794abdcfd527a0f37266862d2bda0cbf
anon-pradip <pradipsubedi360@gmail.com>
2023-04-02 18:55:44 +0545

    make localstorage work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header'
 import AddTask from './components/AddTask'
 import ShowTask from './components/ShowTask'
 
 const App = () => {
-  const [taskList, setTaskList] = useState([])
+  const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem("tasklist")) || [])
   const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(taskList))
+  }, [taskList])
+
   return (
     <div className='bg-slate-200 min-h-screen'>
       <div className='max-w-3xl mx-auto'>
